1fdb0b9cfe7d004db6f9bff211cb7ba4f7cdef72
Chris Jones <chrisjones170187@gmail.com>
2024-04-06 16:45:25 +0100

    editor panel file

---
+++ src/views/editor/editor-panel.tsx
@@ -0,0 +1,29 @@
+import { WorkflowNodeEditorCard } from "@/WorkflowNodeEditorCard";
+import { WorkflowEditorHook } from "@/useWorkflowEditor";
+
+type WorkflowEditorPanelProps = WorkflowEditorHook;
+
+export function WorkflowEditorPanel({
+  isLoading,
+  error,
+  editors,
+  form,
+}: WorkflowEditorPanelProps) {
+  const editorCards = editors?.map((editor) => {
+    return (
+      <WorkflowNodeEditorCard key={editor.nodeId} editor={editor} form={form} />
+    );
+  });
+
+  return (
+    <div className="flex flex-row flex-wrap justify-start gap-2 min-h-[200px]">
+      {isLoading && <div>Loading ComfyUI node definitions...</div>}
+
+      {error && (
+        <div>Error loading ComfyUI node definitions: {error.message}</div>
+      )}
+
+      {editorCards}
+    </div>
+  );
+}
--- src/views/editor/workflow-editor.tsx
+++ src/views/editor/workflow-editor.tsx
@@ -1,39 +1,12 @@
-import { WorkflowNodeEditorCard } from "../../WorkflowNodeEditorCard";
 import { useComfyUIWebsocket } from "../../comfyui/useComfyUIWebsocket";
 import { Sidebar } from "../../components/layout/sidebar";
 import { Button } from "../../components/ui/button";
 import { FormProvider } from "../../components/ui/form";
 
-import { WorkflowEditorHook, useWorkflowEditor } from "../../useWorkflowEditor";
+import { useWorkflowEditor } from "../../useWorkflowEditor";
+import { WorkflowEditorPanel } from "./editor-panel";
 import { JobProgress } from "./job-progress";
 import { PreviewImage } from "./preview-image";
-
-type WorkflowEditorPanelProps = WorkflowEditorHook;
-
-function WorkflowEditorPanel({
-  isLoading,
-  error,
-  editors,
-  form,
-}: WorkflowEditorPanelProps) {
-  const editorCards = editors?.map((editor) => {
-    return (
-      <WorkflowNodeEditorCard key={editor.nodeId} editor={editor} form={form} />
-    );
-  });
-
-  return (
-    <div className="flex flex-row flex-wrap justify-start gap-2 min-h-[200px]">
-      {isLoading && <div>Loading ComfyUI node definitions...</div>}
-
-      {error && (
-        <div>Error loading ComfyUI node definitions: {error.message}</div>
-      )}
-
-      {editorCards}
-    </div>
-  );
-}
 
 export function WorkflowEditorView() {
   const connection = useComfyUIWebsocket();

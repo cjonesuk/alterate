619e9acb6503a64660fd9ac0537ae200b680388d
Chris Jones <chrisjones170187@gmail.com>
2024-05-08 21:01:21 +0100

    wip bad state

--- src/components/image-editor/image-editor.tsx
+++ src/components/image-editor/image-editor.tsx
@@ -1,37 +1,38 @@
 import { ImageReference } from "@/lib/comfyui/images";
-import { Container, Sprite, useApp } from "@pixi/react";
+import { Container, Sprite, Stage, useApp, withPixiApp } from "@pixi/react";
 import * as PIXI from "pixi.js";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Button } from "../ui/button";
 import { useImageReferenceQuery } from "@/lib/image-query";
 import { useBlobObjectUrl } from "@/lib/blob";
 import { useMouseFlow } from "./hooks";
 import { ViewportStage } from "./viewport-stage";
+import { Viewport } from "./viewport";
+import { render } from "react-dom";
+
+const color = 0x0000ff;
 
 const brush = new PIXI.Graphics()
-  .beginFill(0xffffff)
+  .beginFill(color)
   .drawCircle(0, 0, 50)
   .endFill();
 
 // Create a line that will interpolate the drawn points
 const line = new PIXI.Graphics();
 
-function PaintingLayer({ texture }: { texture: PIXI.Texture }) {
-  const app = useApp();
+interface PaintingLayerProps {
+  mask: PIXI.RenderTexture;
+}
 
-  const renderTexture = useMemo(() => {
-    return PIXI.RenderTexture.create({
-      width: texture.width,
-      height: texture.height,
-    });
-  }, [texture]);
+function PaintingLayer({ mask }: PaintingLayerProps) {
+  const app = useApp();
 
   const mouseMove = useCallback(
     (point: PIXI.Point, last: PIXI.Point | null) => {
       brush.position.set(point.x, point.y);
 
       app.renderer.render(brush, {
-        renderTexture,
+        renderTexture: mask,
         clear: false,
         skipUpdateTransform: false,
       });
@@ -42,18 +43,18 @@ function PaintingLayer({ texture }: { texture: PIXI.Texture }) {
       if (last) {
         line
           .clear()
-          .lineStyle({ width: 100, color: 0xffffff })
+          .lineStyle({ width: 100, color: color })
           .moveTo(last.x, last.y)
           .lineTo(point.x, point.y);
 
         app.renderer.render(line, {
-          renderTexture,
+          renderTexture: mask,
           clear: false,
           skipUpdateTransform: false,
         });
       }
     },
-    [app, renderTexture]
+    [app, mask]
   );
 
   const { pointerDown, pointerMove, pointerUp } = useMouseFlow({
@@ -63,7 +64,7 @@ function PaintingLayer({ texture }: { texture: PIXI.Texture }) {
   return (
     <Sprite
       interactive={true}
-      texture={renderTexture}
+      texture={mask}
       onpointerdown={pointerDown}
       onpointerup={pointerUp}
       pointerupoutside={pointerUp}
@@ -77,11 +78,18 @@ export interface ImageEditorProps {
   imageReference: ImageReference;
 }
 
+type ViewportSize = {
+  width: number;
+  height: number;
+};
+
 export function ImageEditor({ onSave, imageReference }: ImageEditorProps) {
   const result = useImageReferenceQuery(imageReference);
   const { url } = useBlobObjectUrl(result.data);
 
   const [texture, setTexture] = useState<PIXI.Texture | null>(null);
+
+  const thingRef = useRef<Stage>(null);
 
   useEffect(() => {
     if (!url) {
@@ -98,18 +106,130 @@ export function ImageEditor({ onSave, imageReference }: ImageEditorProps) {
     });
   }, [url]);
 
+  const renderTexture = useMemo(() => {
+    if (!texture) {
+      return null;
+    }
+
+    return PIXI.RenderTexture.create({
+      width: texture.width,
+      height: texture.height,
+    });
+  }, [texture]);
+
+  const [viewportSize, setViewportSize] = useState<ViewportSize | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const updateParentSize = useCallback(() => {
+    if (!containerRef.current) {
+      return;
+    }
+
+    const size = {
+      width: containerRef.current.clientWidth,
+      height: containerRef.current.clientHeight,
+    };
+
+    console.log("Viewport resize to", size);
+    setViewportSize(size);
+  }, [setViewportSize]);
+
+  useEffect(() => {
+    // Initial call to set parent height
+    updateParentSize();
+
+    // Event listener for resize
+    window.addEventListener("resize", updateParentSize);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener("resize", updateParentSize);
+    };
+  }, [updateParentSize]);
+
+  const ready = viewportSize !== null;
+
+  const handleSave = useCallback(async () => {
+    if (!texture) {
+      console.log("No texture");
+      return;
+    }
+
+    if (!renderTexture) {
+      console.log("No render texture");
+      return;
+    }
+
+    if (!thingRef.current) {
+      console.log("No thingRef");
+      return;
+    }
+
+    console.log("Save");
+
+    // Create a renderer
+    const renderer = PIXI.autoDetectRenderer({
+      width: texture.width,
+      height: texture.height,
+      backgroundColor: 0x000000,
+      backgroundAlpha: 1,
+    });
+
+    console.log("renderer", { renderer, renderTexture });
+
+    const bg = new PIXI.Graphics()
+      .beginFill(0xde3249)
+      .drawRect(0, 0, texture.width, texture.height)
+      .endFill();
+    // const background = new PIXI.Sprite(texture);
+    // background.x = 100;
+    const container = new PIXI.Container();
+    container.addChild(bg);
+    const sprite = new PIXI.Sprite(renderTexture);
+    container.addChild(sprite);
+
+    const rt = renderer.generateTexture(container, {
+      width: texture.width,
+      height: texture.height,
+    });
+
+    rt.on("update", (a) => {
+      console.log("update", a);
+    });
+
+    const d = await renderer.extract.base64(rt);
+    window.open(d, "_blank");
+    console.log("data", d);
+  }, [texture, renderTexture]);
+
   return (
     <div className="flex flex-row">
-      <ViewportStage>
-        {texture && (
-          <Container x={0} y={0}>
-            <Sprite texture={texture} />
-            <PaintingLayer texture={texture} />
-          </Container>
+      <div className="w-full h-full overflow-hidden" ref={containerRef}>
+        {ready && (
+          <Stage
+            ref={thingRef}
+            width={viewportSize.width}
+            height={viewportSize.height}
+            options={{
+              backgroundColor: 0x202020,
+              width: viewportSize.width,
+              height: viewportSize.height,
+              hello: true,
+            }}
+          >
+            <Viewport width={viewportSize.width} height={viewportSize.height}>
+              {texture && renderTexture && (
+                <Container x={0} y={0}>
+                  <Sprite texture={texture} />
+                  <PaintingLayer mask={renderTexture} />
+                </Container>
+              )}
+            </Viewport>
+          </Stage>
         )}
-      </ViewportStage>
+      </div>
       <div className="flex flex-col gap-4 p-4">
-        <Button onClick={onSave}>Save</Button>
+        <Button onClick={handleSave}>Save</Button>
       </div>
     </div>
   );
--- src/components/image-editor/viewport-stage.tsx
+++ src/components/image-editor/viewport-stage.tsx
@@ -1,6 +1,7 @@
 import { Stage } from "@pixi/react";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { Viewport } from "./viewport";
+import { Button } from "../ui/button";
 
 type ViewportSize = {
   width: number;
@@ -14,6 +15,7 @@ interface Props {
 export function ViewportStage({ children }: Props) {
   const [viewportSize, setViewportSize] = useState<ViewportSize | null>(null);
   const containerRef = useRef<HTMLDivElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const updateParentSize = useCallback(() => {
     if (!containerRef.current) {
@@ -42,26 +44,37 @@ export function ViewportStage({ children }: Props) {
     };
   }, [updateParentSize]);
 
-  const ready = viewportSize !== null;
+  const ready = viewportSize !== null && canvasRef.current !== null;
+
+  const handleSave = useCallback(() => {
+    console.log("Save");
+  }, []);
 
   return (
-    <div className="w-full h-full overflow-hidden" ref={containerRef}>
-      {ready && (
-        <Stage
-          width={viewportSize.width}
-          height={viewportSize.height}
-          options={{
-            backgroundColor: 0x202020,
-            width: viewportSize.width,
-            height: viewportSize.height,
-            hello: true,
-          }}
-        >
-          <Viewport width={viewportSize.width} height={viewportSize.height}>
-            {children}
-          </Viewport>
-        </Stage>
-      )}
+    <div className="flex flex-row">
+      <div className="w-full h-full overflow-hidden" ref={containerRef}>
+        <canvas id="something" ref={canvasRef}></canvas>
+        {ready && (
+          <Stage
+            width={viewportSize.width}
+            height={viewportSize.height}
+            options={{
+              backgroundColor: 0x202020,
+              width: viewportSize.width,
+              height: viewportSize.height,
+              hello: true,
+              view: canvasRef.current,
+            }}
+          >
+            <Viewport width={viewportSize.width} height={viewportSize.height}>
+              {children}
+            </Viewport>
+          </Stage>
+        )}
+      </div>
+      <div className="flex flex-col gap-4 p-4">
+        <Button onClick={handleSave}>Save</Button>
+      </div>
     </div>
   );
 }
